aa2da965327809a9fda793bda6973b54d58c2777
Leah Rogers <lero2661@colorado.edu>
2017-11-27 15:17:58 -0700

    figured out user input

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,14 @@
+import java.io.*;
+
 
 public class Game {
 	private GameStatus status;
 	
+	
 	private void displayFirstMessage()
 	{
-		
+		String firstMessage = "Hello, welcome to Notches. Would you like to create a new game or load a saved game? Type create or load.";
+		System.out.println(firstMessage);
 	}
 	
 	private void displayHomeMessage()
@@ -12,13 +16,30 @@ public class Game {
 		
 	}
 	
-	private void getInput()
+	private String getInput() 
 	{
+		InputStreamReader r=new InputStreamReader(System.in);
+		BufferedReader br=new BufferedReader(r);
 		
+		try {
+			return br.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "error";
+		}
 	}
 	
 	public GameStatus getStatus()
 	{
 		return this.status;
 	}
+	
+	public void Play()
+	{
+		displayFirstMessage();
+		System.out.println(getInput());
+	}
+	
+	
 }
